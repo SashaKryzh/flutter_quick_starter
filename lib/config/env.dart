6e702e42f4b39cc290a2c260ca78cceb0113ec7c
Oleksandr Kryzhanovskyi <sashakryzh@gmail.com>
2023-10-31 16:51:13 +0100

    Added env

--- lib/config/env.dart
+++ lib/config/env.dart
@@ -1,1 +1,9 @@
-abstract final class Env {}
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+abstract final class Env {
+  static Future<void> init() async {
+    await dotenv.load();
+  }
+
+  static String get baseUrl => dotenv.env['BASE_URL'] ?? '';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quick_starter/app.dart';
+import 'package:flutter_quick_starter/config/env.dart';
 import 'package:flutter_quick_starter/utils/provider_observer.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Env.init();
 
   runApp(
     const ProviderScope(
